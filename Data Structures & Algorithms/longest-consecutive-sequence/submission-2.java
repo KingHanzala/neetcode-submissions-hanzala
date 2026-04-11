542f1867f13f0fc59061a75c7d24c94177d25939
Hanzala <43098199+KingHanzala@users.noreply.github.com>
2026-04-11 13:26:29 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.java
@@ -0,0 +1,62 @@
+class Solution {
+    class DSU {
+        int n;
+        int [] parent;
+        int [] size;
+        int maxSize;
+
+        DSU(int n){
+            parent = new int[n];
+            size = new int[n];
+            for(int i=0;i<n;i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+            maxSize = 1;
+        }
+
+        public int findParent(int u){
+            if(u == parent[u]) return u;
+            return parent[u] = findParent(parent[u]);
+        }
+
+        public void union(int u, int v) {
+            u = findParent(u);
+            v = findParent(v);
+            if(u == v) return;
+            if (size[v] > size[u]) {
+                parent[u] = v;
+                size[v] = size[u] + size[v];
+                size[u] = size[v];
+            } else {
+                parent[v] = u;
+                size[u] = size[u] + size[v];
+                size[v] = size[u];
+            }
+            maxSize = Math.max(maxSize, size[u]);
+        }
+
+    }
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        DSU d = new DSU(n);
+        Set<Integer> seen = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            int curr = nums[i];
+            if(seen.contains(curr)) continue;
+            if(seen.contains(curr - 1)){
+                d.union(map.get(curr-1), i);
+            }
+            if(seen.contains(curr + 1)){
+                d.union(map.get(curr + 1), i);
+            }
+            seen.add(curr);
+            map.put(curr, i);
+        }
+
+        return d.maxSize;
+        
+    }
+}
