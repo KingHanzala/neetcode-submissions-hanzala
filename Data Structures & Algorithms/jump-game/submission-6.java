b5a064c67294cae1ab7f1e50d2135d2f56320b15
Hanzala <43098199+KingHanzala@users.noreply.github.com>
2026-07-12 23:10:56 +0530

    Add: jump-game - submission-6

---
+++ Data Structures & Algorithms/jump-game/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxReachable = 0;
+        for(int i=0; i<n; i++){
+            if(i<=maxReachable){
+                maxReachable = Math.max(maxReachable, i + nums[i]);
+                if(maxReachable >= n-1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
