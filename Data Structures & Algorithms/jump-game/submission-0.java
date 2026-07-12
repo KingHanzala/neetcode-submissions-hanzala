a773b94202a368e1e1128a4ae09a915f5c79817a
Hanzala <43098199+KingHanzala@users.noreply.github.com>
2026-07-12 23:00:31 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return solve(0, n, nums, dp);
+    }
+
+    public boolean solve(int ind, int n, int[] nums, int[] dp){
+        if(ind > n-1) return false;
+        if(ind == n-1) {
+            dp[ind] = 1;
+            return true;
+        }
+        if(dp[ind]!=-1) return dp[ind] == 1;
+        for(int i=1; i<=nums[ind]; i++){
+            if(solve(ind+i, n, nums, dp)){
+                dp[ind] = 1;
+                return true;
+            }
+        }
+        dp[ind] = 0;
+        return false;
+    }
+}
